c758efb65d6f6bd041f9702f6ff26f31e67492fb
Gabriel Pires <60672959+piresp@users.noreply.github.com>
2021-11-08 22:11:35 -0300

    Add files via upload
    Finalizado o projeto do curso de PDO.

---
+++ php/08_PDO/criar-turma.php
@@ -0,0 +1,36 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$connection = ConnectionCreator::createConnection();
+$studentRepository = new PdoStudentRepository($connection);
+
+// realizo processos de definição da turma
+
+$connection->beginTransaction();
+
+try {
+    $aStudent = new Student(
+        null,
+        'Aerofólio',
+        new DateTimeImmutable('2021-10-01'),
+    );
+    $studentRepository->save($aStudent);
+
+    $anotherStudent = new Student(
+        null,
+        'Gabriel Pires',
+        new DateTimeImmutable('1999-05-01'),
+    );
+    $studentRepository->save($anotherStudent);
+
+    $connection->commit();
+} catch (\PDOException $e) {
+    echo $e->getMessage();
+    $connection->rollBack();
+}
+
